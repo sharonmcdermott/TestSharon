e2c812cb62082fe525b684cae019ba257ce51b90
Sharon's Mac <help@pencilevent.com>
2016-05-27 20:22:56 -0400

    Added FirstViewController and added a storyboard in MainStoryboard. Added a background color.

--- SharonTest/FirstViewController.swift
+++ SharonTest/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirtViewController.swift
 //  SharonTest
 //
 //  Created by Sharon's Mac on 5/27/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
